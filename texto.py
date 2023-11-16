aaaa4333e42a39dc574eccad3cd947f32836173f
seplanas <145620225+seplanas@users.noreply.github.com>
2023-11-15 19:25:52 -0600

    Add files via upload

---
+++ texto.py
@@ -0,0 +1,10 @@
+improt datetime
+import time
+import pandas as pd
+import streamlit as st
+from PIL import Image
+
+st.title("Titulo : Analítica de Datos")
+
+st.header("Este es un header")
+st.subheader("Este es un subheader")
