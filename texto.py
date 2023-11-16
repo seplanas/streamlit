35e2a7548e86d6a3374f8c14167bf61288cde403
seplanas <145620225+seplanas@users.noreply.github.com>
2023-11-15 19:34:17 -0600

    Update texto.py

--- texto.py
+++ texto.py
@@ -8,3 +8,17 @@ st.title("Titulo : Analítica de Datos")
 
 st.header("Este es un header")
 st.subheader("Este es un subheader")
+
+st.text(Texto: Hola streamlit")
+
+st.markdown("# Este es un markdown h1 \n ## Este es un h2 \n ### Este es un h3")
+st.header("Colores de texto y mensajes de error")
+st.success("Successful")
+st.info("Información!")
+st.warning("Warning")
+st.error("Error")
+st.exception("NameError('no está definido')")
+st.header("Obtener información de ayuda de Python")
+st.help(range)
+st.header("Widgets:")
+st.subheader("Checkbox")
